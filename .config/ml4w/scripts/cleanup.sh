1ad15ef6969d1051a9de799fdf9fe8f984ce352e
Glicole <widyanizaz2gmail.com>
2025-03-04 08:24:30 +0700

    aria2, cleanup, starship update

--- .config/ml4w/scripts/cleanup.sh
+++ .config/ml4w/scripts/cleanup.sh
@@ -3,4 +3,23 @@ clear
 aur_helper="$(cat ~/.config/ml4w/settings/aur.sh)"
 figlet -f smslant "Cleanup"
 echo
+echo "Clearing pacman cache"
+pacman_cache_space_used="$(du -sh /var/cache/pacman/pkg/)"
+paccache -r
+echo "Space saved: $pacman_cache_space_used"
+
+echo "Clearing AUR cache"
+aur_cache_space_used="$(du -sh ~/.cache/yay/)"
 $aur_helper -Scc
+echo "Space saved: $aur_cache_space_used"
+
+echo "Removing orphan packages"
+yay -Qdtq | yay -Rns -
+
+# echo "Clearing ~/.cache"
+# home_cache_used="$(du -sh ~/.cache)"
+# rm -rf ~/.cache/
+# echo "Spaced saved: $home_cache_used"
+
+echo "Clearing system logs"
+sudo journalctl --vacuum-time=7d
